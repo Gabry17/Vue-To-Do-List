c27b733484850bde2d88e65ce3f00b1bba995ca1
Gabry17 <gbianchi528@gmail.com>
2022-05-09 15:12:55 +0200

    eseguito milestone 1

--- js/script.js
+++ js/script.js
@@ -3,7 +3,24 @@ const app = new Vue(
     {
         el:'#root',
         data:{
-            message: 'ciao'
+            option:[
+                {
+                    text:'Fare colazione',
+                    done:true
+                },
+                {
+                    text:'Seguire lezione',
+                    done:false
+                },
+                {
+                    text:'Preparare il pranzo',
+                    done:true
+                },
+                {
+                    text:'Fare la spesa',
+                    done:true
+                }
+            ]
         }
     }
 )
